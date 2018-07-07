90fba3258ef39114c6dea5ac09140a2ef13ba848
Jennifer Thompson <thompson.jennifer@gmail.com>
2018-07-07 16:38:05 -0500

    Add simulation of missingness, model fits for MCAR, MNAR + diagnostic plot (comments)

--- simulate_data.R
+++ simulate_data.R
@@ -12,6 +12,8 @@ library(purrr)
 library(furrr)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(ggridges)
 
 ## For furrr functions, set plan (see futures package, furrr README)
 ## Not everything is faster with furrr (eg, first map_dfr()); this is a test :)
@@ -50,21 +52,40 @@ sample_df <- map_dfr(
 ## Need to create a *new* ID variable for grouping later, so that if Pt 1 is
 ## included 3x in sample, their delirium duration isn't tripled!
 
-## -- 2. Calculate P(missingness) for each day, given SOI ----------------------
-## 2a. Simulate random errors: ~ N(0, 1)
-## 2b. Calculate linear predictor for each record
-## 2c. Transform to probability scale (less work for my brain)
-miss_errors <- rnorm(n = nrow(sample_df))
+## -- 2. Calculate P(missingness) for each day, given SOI if not MCAR ----------
+if(miss_type %in% c("mar", "mnar")){
+  ## MAR, MNAR:
+  ## 2a. Simulate random errors: ~ N(0, 1)
+  ## 2b. Calculate linear predictor for each record
+  ## 2c. Transform to probability scale (less work for my brain)
+  miss_errors <- rnorm(n = nrow(sample_df))
+  
+  lp_miss <- int_soi + bsoi * sample_df$sofa_mod + miss_errors
+  sample_df$p_miss <- exp(lp_miss) / (1 + exp(lp_miss))
+  
+  ## Double check
+  ## hist(p_miss)
+  
+  ## 3. Set days with top X% probabilities to missing
+  miss_cutoff <- quantile(sample_df$p_miss, probs = (1 - miss_amt))
+  sample_df$status_miss <- with(sample_df, ifelse(p_miss >= miss_cutoff, NA, status))
+} else{
+  ## MCAR: Set random sample of X% patient-days equal to NA
+  which_na <- sample(1:nrow(sample_df), round(nrow(sample_df) * miss_amt))
+  sample_df$status_miss <- sample_df$status
+  sample_df$status_miss[which_na] <- NA
+}
 
-lp_miss <- int_soi + bsoi * sample_df$sofa_mod + miss_errors
-sample_df$p_miss <- exp(lp_miss) / (1 + exp(lp_miss))
-
-## Double check
-## hist(p_miss)
-
-## 3. Set days with top X% probabilities to missing
-miss_cutoff <- quantile(sample_df$p_miss, probs = (1 - miss_amt))
-sample_df$status_miss <- with(sample_df, ifelse(p_miss >= miss_cutoff, NA, status))
+## If MAR/MNAR, may want to make sure distribution of SOFA is different for
+##  missing, non-missing observations
+ggplot(data = sample_df) +
+  aes(x = sofa_mod, y = is.na(status_miss)) +
+  stat_density_ridges(
+    geom = "density_ridges_gradient", calc_ecdf = TRUE, quantile_lines = TRUE,
+    jittered_points = TRUE,
+    position = position_points_jitter(width = 0.25, height = 0),
+    point_shape = '|', point_size = 3, alpha = 0.25
+  )
 
 ## -- 4. Calculate *summary variable* (days of delirium) for each patient ------
 ## FOR NOW, calculating "missing" version by ignoring missing days (=not delirious).
@@ -75,7 +96,8 @@ del_df <- sample_df %>%
   group_by(id) %>% ## THIS NEEDS TO BE FIXED - see above!
   summarise(
     del_actual = sum(status == "Delirious"),
-    del_miss = sum(status_miss == "Delirious", na.rm = TRUE)
+    del_miss = sum(status_miss == "Delirious", na.rm = TRUE),
+    mean_sofa = mean(sofa_mod) ## no missingness here due to study design
   )
 
 ## -- 5. Simulate outcome | relationship w/ *actual* days delirious ------------
@@ -85,7 +107,15 @@ rbans_errors <- rnorm(n = nrow(del_df))
 del_df$rbans <- int_rbans + bdel * del_df$del_actual + rbans_errors
 
 ## -- 6. Fit model using *delirium with missing data*, extract beta, SD --------
-mod <- lm(rbans ~ del_miss, data = del_df)
-## code to extract beta, SD (from which we'll calculate CI)
+if(miss_type %in% c("mar")){
+  ## MAR: Can adjust for the variable which affects missingness
+  mod <- lm(rbans ~ del_miss + mean_sofa, data = del_df)
+} else{
+  ## MCAR: Don't need to adjust for variable that affects missingness
+  ## MNAR: "Can't" adjust for variable that affects missingness
+  mod <- lm(rbans ~ del_miss, data = del_df)
+}
+
+## Will add code to extract beta, SD (from which we'll calculate CI)
 
 ## More code to save all needed info and return in a list
