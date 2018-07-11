b60ce14a275710d62102c003d13cb068950a0c0e
Jennifer Thompson <thompson.jennifer@gmail.com>
2018-07-11 16:31:44 -0500

    Simulate all outcome data once, save

--- simulate_outcome.R
+++ simulate_outcome.R
@@ -3,6 +3,9 @@
 ## a given data.frame, given a specified relationship between delirium/RBANS
 ################################################################################
 
+library(purrr)
+library(dplyr)
+
 ## Assumes `df` has columns `new_id` (subject + rep ID) and `status` (which
 ## takes values Normal, Delirious, Comatose) and contains >=1 record per subject
 
@@ -14,13 +17,10 @@
 
 ## intcs: default = E(cogscore) for pt with no delirium in motivating example
 
-library(dplyr)
-
 simulate_cogscore <- function(
   df, ## Starting longitudinal data.frame
   bdel, ## average change in outcome for a one-day increase in delirium
-  intcs = 79.7103 ## mean outcome for patients with no delirium
-    ## default value comes from motivating example
+  intcs = 120 ## max score possible; should yield positive values in simulation
 ){
   npts <- length(unique(df$new_id))
   
@@ -36,7 +36,27 @@ simulate_cogscore <- function(
     ungroup()
 
   outcome_df$cogscore <- intcs + bdel * outcome_df$del_actual + epsilon
+  outcome_df$true_beta <- bdel
   
   return(outcome_df)
 
 }
+
+## -- Simulate all datasets; save in analysisdata/ -----------------------------
+## For each dataset created in `simulate_data.R`, want corresponding dfs with
+## betas for delirium vs scores of -1, -3, -5 (~ one-, three-, five-point drops
+## in outcome for each day of delirium)
+
+simdata_list <- readRDS("analysisdata/simdata.rds")
+
+## Get all combinations
+outcome_args <- cross2(.x = simdata_list, .y = c(-1, -3, -5)) %>%
+  map(set_names, c("df", "bdel"))
+
+## furrr *not* faster in this case (17.225sec vs 16.615)
+simoutcomes_list <- map(
+  outcome_args,
+  ~ simulate_cogscore(df = pluck(., "df"), bdel = pluck(., "bdel"))
+)
+
+saveRDS(simoutcomes_list, file = "analysisdata/outcomedata.rds")
