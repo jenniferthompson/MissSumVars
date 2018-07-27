1de021578cca6580e77e00a0ab0652c8df34cecc
Jennifer Thompson <thompson.jennifer@gmail.com>
2018-07-27 17:55:24 -0500

    Fixes for labels, factors

--- seed_prep.R
+++ seed_prep.R
@@ -10,6 +10,22 @@ library(dplyr)
 
 ## Data was downloaded from safe storage spot, will be deleted once we're done
 load("../Downloads/seeddata.Rdata")
+## Remove variable labels; they are causing pain
+## Thanks, StackOverflow https://stackoverflow.com/questions/2394902/remove-variable-labels-attached-with-foreign-hmisc-spss-import-functions
+clear.labels <- function(x) {
+  if(is.list(x)) {
+    for(i in 1 : length(x)) class(x[[i]]) <- setdiff(class(x[[i]]), 'labelled') 
+    for(i in 1 : length(x)) attr(x[[i]],"label") <- NULL
+  }
+  else {
+    class(x) <- setdiff(class(x), "labelled")
+    attr(x, "label") <- NULL
+  }
+  return(x)
+}
+brain.oneobs <- clear.labels(brain.oneobs)
+brain.daily <- clear.labels(brain.daily)
+brain.fu <- clear.labels(brain.fu)
 
 ## -- Which patients have 12m followup? ----------------------------------------
 ## These patients are likely different than the ones who don't survive/aren't
@@ -36,10 +52,14 @@ save_df$status <- as.character(save_df$status)
 ## Save final file used for simulations
 # saveRDS(save_df, file = "rawdata/seeddf.rds")
 
-## Save "real" data for "real" analyses
+## -- Save "real" data for "real" analyses -------------------------------------
+## Keep only patients with long-term outcomes; keep only in-hospital days
+## (status is *not* discharged, deceased, withdrawn)
 study1_daily <- subset(
   brain.daily,
-  id %in% ids_lt,
+  id %in% ids_lt &
+    (is.na(status.today) |
+       status.today %in% c("Normal", "Delirious", "Comatose")),
   select = c(id, study.day, daily.sofa.mod.locf, status.today)
 )
 names(study1_daily) <- c("id", "study_day", "sofa_mod", "status")
@@ -54,6 +74,7 @@ study1_fu$id <- match(study1_fu$id, ids_lt)
 
 study1_df <- merge(study1_daily, study1_fu, by = "id", all.x = TRUE)
 names(study1_df)[ncol(study1_df)] <- "outcome"
+study1_df$status <- as.character(study1_df$status)
 
 saveRDS(study1_df, file = "rawdata/study1.rds")
 
@@ -61,6 +82,9 @@ saveRDS(study1_df, file = "rawdata/study1.rds")
 
 ## Data was downloaded from safe storage spot, will be deleted once we're done
 load("../Downloads/seeddata2.Rdata")
+
+tbi.oneobs <- clear.labels(tbi.oneobs)
+tbi.daily <- clear.labels(tbi.daily)
 
 ## Anonymize IDs
 mrns <- sort(unique(tbi.oneobs$mrn))
@@ -75,6 +99,7 @@ study2_daily <- subset(
 names(study2_daily) <- c("id", "study_day", "status", "sofa_mod")
 
 ## Only keep patients with a FIM score (hospital survivors)
+## No need to restrict statuses; this dataset only has in-hospital days
 has_fim <- subset(tbi.oneobs, !is.na(fim.total))$id
 
 study2_daily <- subset(study2_daily, id %in% has_fim)
@@ -83,5 +108,6 @@ study2_oneobs <- subset(tbi.oneobs, !is.na(fim.total), select = c(id, fim.total)
 names(study2_oneobs) <- c("id", "outcome")
 
 study2_df <- merge(study2_daily, study2_oneobs, by = "id")
+study2_df$status <- as.character(study2_df$status)
 
 saveRDS(study2_df, file = "rawdata/study2.rds")
